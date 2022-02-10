c54600864a2f8f82226c45778a6d35ed695fb47c
Ramakrishna Janapureddy <developer.raakhi005@gmail.com>
2022-02-10 10:41:49 +0530

    packaging changed to war

--- src/main/java/com/orgofarmsgroup/DiscoveryServiceMsStarter.java
+++ src/main/java/com/orgofarmsgroup/DiscoveryServiceMsStarter.java
@@ -3,13 +3,19 @@ package com.orgofarmsgroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableEurekaServer
-public class DiscoveryServiceMsStarter {
+public class DiscoveryServiceMsStarter extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServiceMsStarter.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DiscoveryServiceMsStarter.class);
     }
 }
